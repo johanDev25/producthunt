66877366a85a28f750100b62b6e40cdd4bf7eb0c
Johan <johan225@hotmail.es>
2019-09-10 14:15:49 -0500

    Agrega la funcionalidad de deslogearse

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,20 @@
 class SessionsController < ApplicationController
-  def new
-  end
+	def new
+	end
 
-  def create
-  user = User.find_by(email: params[:email])
-  if user && user.authenticate(params[:password])
-    sign_in(user)
-    redirect_to root_path
-  else
-    render :new
-  end
-end
+	def create
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			sign_in(user)
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_path
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   post 'login', to: 'sessions#create'
 
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users, only: [:new, :create]
 
   resources :products
