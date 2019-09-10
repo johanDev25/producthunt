01b1907d129c8e71cfe8bfc5488c189731d1af8a
Johan <johan225@hotmail.es>
2019-09-10 16:56:15 -0500

    Agrega usuarios a productos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_183803) do
+ActiveRecord::Schema.define(version: 2019_09_10_214230) do
 
   create_table "products", force: :cascade do |t|
     t.string "name", limit: 50
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_09_10_183803) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 2019_09_10_183803) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "products", "users"
 end
