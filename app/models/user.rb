7b8a208fa2ce13846e230e0ab44d2dd32b9c81d2
Johan <johan225@hotmail.es>
2019-09-10 16:25:41 -0500

    Agrega valdiaciones a User

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,13 @@
 #
 
 class User < ApplicationRecord
+	validates :email, uniqueness: true, format: { with: /@/,
+    message: "Debe contener un arroba" }
+	validates :password_digest, presence: true, on: :create
+	validates :password_digest, length: { in: 6..20 }, allow_nil: true
+	validates :name, presence: true
+
 	has_secure_password
 	has_secure_password validation: false
+
 end
